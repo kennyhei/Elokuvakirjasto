ffe447de6247a876a858bfeda6e87761d238ce5b
Kenny Heinonen <kenny_heinonen@hotmail.com>
2017-01-04 20:04:03 +0200

    Use HTTPS

--- web/app/services/omdb_service.js
+++ web/app/services/omdb_service.js
@@ -1,6 +1,6 @@
 MovieApp.service('OMDbService', function($http) {
 
     this.findMovies = function(name, year) {
-        return $http.get('http://www.omdbapi.com', { params: { s: name, y: year } });
+        return $http.get('https://www.omdbapi.com', { params: { s: name, y: year } });
     }
 });
