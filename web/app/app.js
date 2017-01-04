0ea0ea908d4d77d8067c2954198eec0d83474069
Kenny Heinonen <kenny_heinonen@hotmail.com>
2017-01-04 17:59:56 +0200

    Define routes for adding, editing, viewing and listing movies and create template views

--- web/app/app.js
+++ web/app/app.js
@@ -1,1 +1,26 @@
 // Toteuta moduulisi tänne
+var MovieApp = angular.module('MovieApp', ['ngRoute', 'firebase']);
+
+MovieApp.config(function ($routeProvider) {
+
+    $routeProvider
+    .when('/movies', {
+        controller: 'ListMoviesController',
+        templateUrl: 'app/views/list_movies.html'
+    })
+    .when('/movies/new', {
+        controller: 'AddMovieController',
+        templateUrl: 'app/views/add_movie.html'
+    })
+    .when('/movies/:key', {
+        controller: 'ShowMovieController',
+        templateUrl: 'app/views/show_movie.html'
+    })
+    .when('/movies/:key/edit', {
+        controller: 'EditMovieController',
+        templateUrl: 'app/views/edit_movie.html'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+});
