976abdea524d0cd5ba84ae99b7643124a9d146f1
Kenny Heinonen <kenny_heinonen@hotmail.com>
2017-01-04 20:11:42 +0200

    Show only movies found from OMDb

--- web/app/controllers/search_movies_controller.js
+++ web/app/controllers/search_movies_controller.js
@@ -8,7 +8,9 @@ MovieApp.controller('SearchMoviesController', function($scope, OMDbService) {
             year  = $scope.year;
 
         OMDbService.findMovies(title, year).then(function (results) {
-            $scope.movies = results.data.Search;
+            $scope.movies = results.data.Search.filter(function (movie) {
+                return movie.Type === 'movie';
+            });
             $scope.searched = true;
         });
     }
